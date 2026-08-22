2c45c754a9fdf39b74e68563a32adaaa7523cc29
Alán F. Muñoz <afer.mg@gmail.com>
2026-08-22 08:36:46 -0400

    Increase server receive timeout

--- server.py
+++ server.py
@@ -158,7 +158,7 @@ def process(
 
 
 async def main():
-    with pynng.Rep0(listen=address, recv_timeout=300) as sock:
+    with pynng.Rep0(listen=address, recv_timeout=300_000) as sock:
         print(f"StarDist server listening on {address}", flush=True)
         async with trio.open_nursery() as nursery:
             responder_curried = partial(responder, setup=setup)
